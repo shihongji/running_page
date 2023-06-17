cf8c413c11312e150612de24ee260cc2ff6b460b
hongji <shihongji21@gmail.com>
2023-06-17 12:20:18 -0700

    change map style to satellite

--- src/components/RunMap/index.jsx
+++ src/components/RunMap/index.jsx
@@ -77,7 +77,7 @@ const RunMap = ({
       {...viewport}
       width="100%"
       height={MAP_HEIGHT}
-      mapStyle="mapbox://styles/mapbox/dark-v10"
+      mapStyle="mapbox://styles/mapbox/satellite-v9"
       onViewportChange={setViewport}
       ref={mapRefCallback}
       mapboxApiAccessToken={MAPBOX_TOKEN}
